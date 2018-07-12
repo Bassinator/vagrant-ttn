fe5437bc2f1ce8b993cdd8eea2ce157b43d446e0
Bastian Bukatz <bastian.greuel@gmail.com>
2018-07-12 14:54:39 +0200

    cookies to be stored in tmp folder

--- library/ttn-login.sh
+++ library/ttn-login.sh
@@ -7,9 +7,9 @@ source $1;
 USERNAME=$username
 PASSWORD=$password
 
-wget --save-cookies cookies --keep-session-cookies --post-data="username=$USERNAME&password=$PASSWORD" https://account.thethingsnetwork.org/api/v2/users/login -O - &> /dev/null;
+wget --save-cookies /tmp/cookies --keep-session-cookies --post-data="username=$USERNAME&password=$PASSWORD" https://account.thethingsnetwork.org/api/v2/users/login -O - &> /dev/null;
 
-TOKEN=`wget --load-cookies cookies --max-redirect 0 'https://account.thethingsnetwork.org/users/authorize?client_id=ttnctl&redirect_uri=/oauth/callback/ttnctl&response_type=code'  2>&1  | perl -lne 'print $1 if /^.*code=([^\s]*)/';`
+TOKEN=`wget --load-cookies /tmp/cookies --max-redirect 0 'https://account.thethingsnetwork.org/users/authorize?client_id=ttnctl&redirect_uri=/oauth/callback/ttnctl&response_type=code'  2>&1  | perl -lne 'print $1 if /^.*code=([^\s]*)/';`
 
 if ttnctl user login $TOKEN; then
    ACCOUNT=`ttnctl user | perl -lne 'print $1 if /^.*Username: ([^\s]*)/';`
